80fb77fb88d57239e0352d00ff3cbe4a4810dd76
Mohamed Hussain H <83116894+hussain033@users.noreply.github.com>
2023-03-21 20:59:14 +0530

    update api.py

--- api.py
+++ api.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI, Query
 import uvicorn
 from predict import prediction, preprocess
 from typing import List
+from mangum import Mangum
 
 
 app = FastAPI()
+handler = Mangum(app)
 
 @app.get('/predict')
 async def diseaseApi(dataFrame: List[str] = Query(None)):
